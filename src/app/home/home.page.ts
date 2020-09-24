9dd260141c58693cc4ab4109ac87d148ad6dddb4
jietan97 <33015141+jietan97@users.noreply.github.com>
2020-09-24 15:44:54 +0800

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController, Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, private platform: Platform) {
+ 
+  }
+ 
+  showPlatform() {
+    let text = 'I run on: ' + this.platform.platforms();
+    console.log(text)
+  }
+}
